a0a9af1c98dc65d7236a516519b1fee54bba238f
cprates <cpr4t3s@gmail.com>
2019-12-27 11:58:26 +0000

    Move exec func to the right file

--- runtime/create.go
+++ runtime/create.go
@@ -1,14 +1,11 @@
 package runtime
 
 import (
-	"encoding/json"
 	"fmt"
 	"os"
 	"os/exec"
 	"path"
 	"path/filepath"
-	"strconv"
-	"syscall"
 
 	"github.com/cprates/box/spec"
 
@@ -139,72 +136,4 @@ func (b *boxRuntime) includeExecFifo(cmd *exec.Cmd) error {
 	cmd.ExtraFiles = append(cmd.ExtraFiles, os.NewFile(uintptr(fifoFd), fifoName))
 	cmd.Env = append(cmd.Env, fmt.Sprintf("BOX_FIFO_FD=%d", stdioFdCount+len(cmd.ExtraFiles)-1))
 	return nil
-}
-
-func (b *boxRuntime) start() (err error) {
-	cmd := exec.Command("/proc/self/exe", "bootstrap")
-	// TODO: set IO correctly
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS |
-			syscall.CLONE_NEWPID |
-			syscall.CLONE_NEWNS |
-			syscall.CLONE_NEWIPC |
-			syscall.CLONE_NEWNET,
-		//syscall.CLONE_NEWUSER,
-		Unshareflags: syscall.CLONE_NEWNS,
-	}
-
-	configRPipe, configWPipe, err := os.Pipe()
-	if err != nil {
-		err = fmt.Errorf("creating configPipe: %s", err)
-		return
-	}
-	defer configWPipe.Close()
-	defer configRPipe.Close()
-
-	cmd.ExtraFiles = append(cmd.ExtraFiles, os.NewFile(configRPipe.Fd(), "configPipe"))
-	configFd := stdioFdCount + len(cmd.ExtraFiles) - 1
-
-	cmd.Env = []string{
-		"BOX_BOOTSTRAP_CONFIG_FD=" + strconv.Itoa(configFd),
-		"BOX_BOOTSTRAP_LOG_FD=" + strconv.Itoa(int(os.Stdout.Fd())), // TODO: handle logging properly
-		"BOX_DEBUG=" + os.Getenv("BOX_DEBUG"),
-	}
-
-	// send box config
-	if err = json.NewEncoder(configWPipe).Encode(&b.childProcess.config); err != nil {
-		err = fmt.Errorf("sending config to child: %s", err)
-		return
-	}
-
-	if err = b.includeExecFifo(cmd); err != nil {
-		err = fmt.Errorf("including fifo fd: %s", err)
-		return
-	}
-
-	if err = cmd.Start(); err != nil {
-		err = fmt.Errorf("starting child: %s", err)
-		return
-	}
-
-	b.childProcess.pid = cmd.Process.Pid
-	b.childProcess.state = StateCreated
-
-	// TODO: delete after storing this in a state file
-	log.Printf("Box instance PID:::::: %d\n\n", cmd.Process.Pid)
-
-	// TODO
-	//go func() {
-	//	// TODO: Not sure but the doc string state that cmd.Wait releases resources, but this
-	//	//   will never return because this process will finish before the child
-	//	if err = cmd.Wait(); err != nil {
-	//		err = fmt.Errorf("box: bootstrapping box instance: %s", err)
-	//		return
-	//	}
-	//}()
-
-	return
 }
--- runtime/start.go
+++ runtime/start.go
@@ -1,5 +1,16 @@
 package runtime
 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/exec"
+	"strconv"
+	"syscall"
+
+	log "github.com/sirupsen/logrus"
+)
+
 func (b *boxRuntime) Start(pid int) error {
 	// TODO: currently there is no way to know if the child process has died for sure.
 	//  runC uses the start time of the process to make sure it is the right process
@@ -23,4 +34,72 @@ func (b *boxRuntime) Start(pid int) error {
 	}
 
 	return b.exec()
+}
+
+func (b *boxRuntime) start() (err error) {
+	cmd := exec.Command("/proc/self/exe", "bootstrap")
+	// TODO: set IO correctly
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS |
+			syscall.CLONE_NEWPID |
+			syscall.CLONE_NEWNS |
+			syscall.CLONE_NEWIPC |
+			syscall.CLONE_NEWNET,
+		//syscall.CLONE_NEWUSER,
+		Unshareflags: syscall.CLONE_NEWNS,
+	}
+
+	configRPipe, configWPipe, err := os.Pipe()
+	if err != nil {
+		err = fmt.Errorf("creating configPipe: %s", err)
+		return
+	}
+	defer configWPipe.Close()
+	defer configRPipe.Close()
+
+	cmd.ExtraFiles = append(cmd.ExtraFiles, os.NewFile(configRPipe.Fd(), "configPipe"))
+	configFd := stdioFdCount + len(cmd.ExtraFiles) - 1
+
+	cmd.Env = []string{
+		"BOX_BOOTSTRAP_CONFIG_FD=" + strconv.Itoa(configFd),
+		"BOX_BOOTSTRAP_LOG_FD=" + strconv.Itoa(int(os.Stdout.Fd())), // TODO: handle logging properly
+		"BOX_DEBUG=" + os.Getenv("BOX_DEBUG"),
+	}
+
+	// send box config
+	if err = json.NewEncoder(configWPipe).Encode(&b.childProcess.config); err != nil {
+		err = fmt.Errorf("sending config to child: %s", err)
+		return
+	}
+
+	if err = b.includeExecFifo(cmd); err != nil {
+		err = fmt.Errorf("including fifo fd: %s", err)
+		return
+	}
+
+	if err = cmd.Start(); err != nil {
+		err = fmt.Errorf("starting child: %s", err)
+		return
+	}
+
+	b.childProcess.pid = cmd.Process.Pid
+	b.childProcess.state = StateCreated
+
+	// TODO: delete after storing this in a state file
+	log.Printf("Box instance PID:::::: %d\n\n", cmd.Process.Pid)
+
+	// TODO
+	//go func() {
+	//	// TODO: Not sure but the doc string state that cmd.Wait releases resources, but this
+	//	//   will never return because this process will finish before the child
+	//	if err = cmd.Wait(); err != nil {
+	//		err = fmt.Errorf("box: bootstrapping box instance: %s", err)
+	//		return
+	//	}
+	//}()
+
+	return
 }
